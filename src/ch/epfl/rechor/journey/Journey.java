90df19addcc651ee60c7d2de363ecebaac41067c
axel <axel.verga@epfl.ch>
2025-02-20 21:37:34 +0100

    Refactoring

--- src/ch/epfl/rechor/journey/Journey.java
+++ src/ch/epfl/rechor/journey/Journey.java
@@ -1,5 +1,7 @@
 package ch.epfl.rechor.journey;
 
+import ch.epfl.rechor.Preconditions;
+
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -9,25 +11,23 @@ public record Journey(List<Leg> legs) {
 
     // Constructeur compact
     public Journey {
+
+
         // Condition 1 : La liste ne doit pas être nulle
         Objects.requireNonNull(legs, "legs is null");
 
         // Le reste des conditions se trouvent au sein de la boucle
         // Pour chaque étapes consécutives :
         for (int i = 1; i < legs.size(); i++) {
+
             // Condition 2 : Les étapes doivent s'alterner
-            if (legs[i].getClass() == legs[i - 1].getClass()){
-                throw new IllegalArgumentException();
-            }
-            // Condition 3 : Sauf début, l'instant de départ est après le départ du précédent
-            if (legs[i].depTime().isBefore(legs[i - 1].depTime())){
-                throw new IllegalArgumentException();
-            }
+            Preconditions.checkArgument(legs.get(i).getClass() != legs.get(i - 1).getClass());
+
+            // Condition 3 : Sauf début, l'instant de départ est après l'arrivée du précédent
+            Preconditions.checkArgument(legs.get(i).depTime().isBefore(legs.get(i - 1).arrTime()));
+
             // Condition 4 : Sauf début, le départ = l'arivée des étapes précédentes
-            // La méthode compareTo renvoie 0 si elles sont égales, et c'est ce qu'on veut
-            if (legs[i].depTime().compareTo(legs[i - 1].arrTime()) != 0){
-                throw new IllegalArgumentException();
-            }
+            Preconditions.checkArgument(!legs.get(i).depTime().isEqual(legs.get(i - 1).arrTime()));
         }
 
         // Si toutes les conditions sont validées
@@ -39,16 +39,31 @@ public record Journey(List<Leg> legs) {
     // TODO : "Journey offre ces classes publiques. Mais Leg aussi ??
     // Oui car c'est différent, ici c'est voyage alors que dans Leg
     // c'est étape ! donc refaire les trucs ici aussi.
-//    public Stop depStop(){}
-//
-//    public Stop arrStop(){};
-//
-//    public LocalDateTime depTime(){};
-//
-//    public LocalDateTime arrTime(){};
-//
-//    public Duration duration(){};
 
+    // Retourne l'arrêt de départ du voyage, c.-à-d. celui de sa première étape,
+    public Stop depStop() {
+        return legs.getFirst().depStop();
+    }
+
+    // Retourne l'arrêt d'arrivée du voyage, c.-à-d. celui de sa dernière étape,
+    public Stop arrStop() {
+        return legs.getLast().arrStop();
+    };
+
+    // retourne la date/heure de début du voyage, c.-à-d. celle de sa première étape,
+    public LocalDateTime depTime() {
+        return legs.getFirst().depTime();
+    };
+
+    // retourne la date/heure de fin du voyage, c.-à-d. celle de sa dernière étape,
+    public LocalDateTime arrTime() {
+        return legs.getLast().arrTime();
+    };
+
+    // etourne la durée totale du voyage, c.-à-d. celle séparant la date/heure de fin de celle de début.
+    public Duration duration() {
+        return Duration.between(legs.getFirst().depTime(), legs.getLast().arrTime());
+    };
 
     public interface Leg {
 
