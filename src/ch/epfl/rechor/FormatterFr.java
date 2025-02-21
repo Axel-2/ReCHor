cbfd0c4570f002feff5093f7992213f031d9ced7
axel <axel.verga@epfl.ch>
2025-02-21 09:23:33 +0100

    Formatter avancement

--- src/ch/epfl/rechor/FormatterFr.java
+++ src/ch/epfl/rechor/FormatterFr.java
@@ -16,12 +16,15 @@ public final class FormatterFr {
     public static String formatDuration(Duration duration) {
 
         DateTimeFormatter fmt = new DateTimeFormatterBuilder()
-                .appendValue(ChronoField.HOUR_OF_AMPM)
-                .appendLiteral('/')
+                .appendValue(ChronoField.MINUTE_OF_DAY)
+                .appendLiteral(" min")
                 .appendValue(ChronoField.MONTH_OF_YEAR)
                 .appendLiteral('/')
                 .appendValue(ChronoField.YEAR)
                 .toFormatter();
+
+
+
 
         return "s";
 
@@ -50,19 +53,64 @@ public final class FormatterFr {
 
     public static String formatLeg(Journey.Leg.Foot footLeg) {
 
+        // changement (5 min)
+        // trajet à pied (3 min)
+
         return "s";
 
     }
 
     public static String formatLeg(Journey.Leg.Transport leg) {
-        return "s";
 
+        // 16h26 Renens VD (voie 4) → Lausanne (arr. 16h33 voie 5)
+        DateTimeFormatter fmt = new DateTimeFormatterBuilder()
+                .appendValue(ChronoField.HOUR_OF_DAY)
+                .appendLiteral("h")
+                .appendValue(ChronoField.MINUTE_OF_HOUR, 2)
+                .toFormatter();
+
+        // depTime
+        String depTimeString = fmt.format(leg.depTime());
+        String arrTimeString = fmt.format(leg.arrTime());
+
+        StringBuilder builder = new StringBuilder()
+                .append(depTimeString)
+                .append(" ")
+                .append(leg.depStop().name());
+
+
+        if (leg.depStop().platformName() != null) {
+            builder.append(" (voie ")
+                    .append(leg.depStop().platformName())
+                    .append(")");
+        }
+
+        builder.append(" → ")
+                .append(leg.arrStop().name())
+                .append(" (arr. ")
+                .append(arrTimeString);
+
+                if (leg.arrStop())
+                .append(" voie ")
+                .append(leg.arrStop().platformName())
+                .append(")")
+                .toString();
+
+        return builder.toString();
 
     }
 
     public static String formatRouteDestination(Journey.Leg.Transport transportLeg) {
 
-        return "s";
+        // IR 15 Direction Luzern
+
+        StringBuilder string = new StringBuilder()
+                .append(transportLeg.route())
+                .append(" Direction ")
+                .append(transportLeg.destination());
+
+
+        return string.toString();
 
 
     }
