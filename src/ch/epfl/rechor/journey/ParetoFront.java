50a9383c0b521a3c2dea9f7c1996db598bd7ba33
Blaazen <blqzen@gmail.com>
2025-02-28 11:31:29 +0100

    Avant la pause manger

---
+++ src/ch/epfl/rechor/journey/ParetoFront.java
@@ -0,0 +1,122 @@
+package ch.epfl.rechor.journey;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.function.LongConsumer;
+
+/**
+ * Frontière de Pareto
+ * @author Yoann Salamin (390522)
+ * @author Axel Verga (398787)
+ */
+public final class ParetoFront {
+
+    private long[] packed_criterias;
+    public static final ParetoFront EMPTY = new ParetoFront(new long[0]);
+
+    private ParetoFront(long[] packed_criterias){
+        this.packed_criterias = packed_criterias;
+        // TODO La méthode build  doit garantir ...
+    }
+
+    /**
+     * Retourne la taille de la frontière de Pareto
+     * @return la taille (int)
+     */
+    public int size(){
+        return packed_criterias.length;
+    }
+
+    /**
+     * Retourne les critères d'optimisation empaquetés dont l'heure d'arrivée et le nombre de changements sont ceux donnés
+     * @param arrMins entier représentant le nombre de minutes passées depuis minuit
+     * @param changes entier représentant le nombre de changements
+     * @return le critère correspondant (long)
+     */
+    public long get(int arrMins, int changes){
+
+        for (long pc : packed_criterias){
+            if(PackedCriteria.arrMins(pc) == arrMins &&
+               PackedCriteria.changes(pc) == changes)
+            {
+                return pc;
+            }
+        }
+        throw new NoSuchElementException("Aucun long ne contient ces données");
+    }
+
+    /**
+     * Appelle la méthode accept de l'action de type LongConsumer donnée avec chacun des critères de la frontière
+     * @param action action
+     */
+    public void forEach(LongConsumer action){
+        for (long packed_criteria : packed_criterias){
+            action.accept(packed_criteria);
+        }
+    }
+
+    /**
+     * Méthode toString basique
+     * @return la chaîne de caractère décrivant au mieux la classe
+     */
+    public String toString(){
+        StringBuilder s = new StringBuilder();
+
+        for (long pc : packed_criterias){
+
+            if(PackedCriteria.hasDepMins(pc)){
+                s.append("Heure de départ : ")
+                        .append(PackedCriteria.depMins(pc));
+            }
+
+            s.append("\n Heure d'arrivée : ")
+                    .append(PackedCriteria.arrMins(pc))
+                    .append("\n Changements : ")
+                    .append(PackedCriteria.changes(pc));
+        }
+
+        return s.toString();
+    }
+
+    /**
+     * Builder statiquement imbriqué
+     */
+    public final static class Builder{
+
+        // Tableau de type long qui contient les tuples
+        private long[] arrayInConstruction;
+        private int effectiveSize;
+
+        /**
+         * Constructeur par défaut
+         */
+        public Builder(){
+            // TODO pas sûr de la taille
+            this.arrayInConstruction = new long[2];
+            this.effectiveSize =0;
+        }
+
+        /**
+         * Constructeur de copie
+         * @param that objet que l'on veut copier
+         */
+        public Builder(Builder that){
+            this.arrayInConstruction = that.arrayInConstruction.clone();
+            this.effectiveSize = that.effectiveSize;
+
+        }
+
+        /**
+         * Retourne true si le tableau en cours de construction est vide, false sinon
+         * @return (booléen)
+         */
+        public boolean isEmpty(){
+            return arrayInConstruction.length == 0;
+        }
+
+        public Builder clear(){
+            return null;
+        }
+    }
+
+}
---
+++ test/ch/epfl/rechor/journey/MyParetoFrontTest.java
@@ -0,0 +1,44 @@
+package ch.epfl.rechor.journey;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyParetoFrontTest {
+    // TODO faire des tests alors que constructeur privé..
+
+    @Test
+    void size() {
+
+        // S'assure que EMPTY est bien de taille zero
+        assertEquals(0,ParetoFront.EMPTY.size());
+
+
+    }
+
+    @Test
+    void get() {
+
+        // Get un tableau EMPTY lève l'exception
+        assertThrows(NoSuchElementException.class, () -> {
+            ParetoFront.EMPTY.get(600, 2);
+        });
+    }
+
+    @Test
+    void forEach() {
+
+        // ForEach de EMPTY donne une liste vide
+        List<Long> collected = new ArrayList<>();
+        ParetoFront.EMPTY.forEach(value -> {collected.add(value);});
+        assertTrue(collected.isEmpty());
+    }
+
+    @Test
+    void testToString() {
+    }
+}
