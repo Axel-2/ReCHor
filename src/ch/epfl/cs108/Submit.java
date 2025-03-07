8ebf1a1295c7771b3bcb975ecde7399ffdc878da
axel <axel.verga@epfl.ch>
2025-03-07 11:00:36 +0100

    sdfad

--- src/ch/epfl/cs108/Submit.java
+++ src/ch/epfl/cs108/Submit.java
@@ -31,7 +31,7 @@ public final class Submit {
     // -------------
     // Jeton du premier membre du groupe
     // Axel
-    private static final String TOKEN_1 = "Uo8AX5ei";
+    private static final String TOKEN_1 = "eihieZ9n";
     // Jeton du second membre (identique au premier pour les personnes travaillant seules)
     private static final String TOKEN_2 = "Uovo6oem";
     // Yoann
--- src/ch/epfl/rechor/timetable/mapped/BufferedStations.java
+++ src/ch/epfl/rechor/timetable/mapped/BufferedStations.java
@@ -3,10 +3,40 @@ package ch.epfl.rechor.timetable.mapped;
 
 import ch.epfl.rechor.timetable.Stations;
 
+import java.nio.ByteBuffer;
+import java.util.List;
+
+
+/**
+ * Classe qui implémente l'interface Stations et permet d'accéder
+ * à une table de gares représentée de manière aplatie
+ * @author Yoann Salamin (390522)
+ * @author Axel Verga (398787)
+ */
 public final class BufferedStations implements Stations {
+
+    private int NAME_ID = 0;
+    private int LON = 1;
+    private int LAT = 2;
+
+    Structure stationStructure = new Structure(
+            field(NAME_ID, U16),
+            field(LON, S32),
+            field(LAT, S32)
+    );
+
+
+
+
+    // unique constructeur publlique
+    public BufferedStations(List<String> stringTable, ByteBuffer buffer) {
+
+
+    }
+
+
     /**
      * Retourne le nom de la gare d'index donné
-     *
      * @param id index d'une gare
      * @return le nom de la gare d'index donné
      * @throws IndexOutOfBoundsException Erreur si l'index est invalide
@@ -17,7 +47,7 @@ public final class BufferedStations implements Stations {
     }
 
     /**
-     * Fonctio qui retourne la longitude, en degrés, de la gare d'index donné
+     * Fonction qui retourne la longitude, en degrés, de la gare d'index donné
      *
      * @param id index d'une gare
      * @return longitude, en degrés, de la gare d'index donné
