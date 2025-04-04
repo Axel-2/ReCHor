d5c5c1f556563ec0a3019c966db3cb1e030d6171
Blaazen <blqzen@gmail.com>
2025-04-04 08:51:52 +0200

    commit

--- src/ch/epfl/rechor/journey/Router.java
+++ src/ch/epfl/rechor/journey/Router.java
@@ -4,6 +4,7 @@ import ch.epfl.rechor.timetable.Connections;
 import ch.epfl.rechor.timetable.mapped.FileTimeTable;
 
 import java.time.LocalDate;
+import java.util.NoSuchElementException;
 
 /**
  * Classe qui représente un "Routeur" c.-à-d. un objet
@@ -27,8 +28,45 @@ public record Router(FileTimeTable timetable) {
         Profile.Builder profileBuilder = new Profile.Builder(timetable, date, arrStationId);
 
         // Algorithme CSA
-        // 1) On parcourt la totalité des liaisons de l'horaire, dans l'ordre décroissant
+        // On parcourt la totalité des liaisons de l'horaire, dans l'ordre décroissant
         for(int i = timetable.connectionsFor(date).size() - 1; i >= 0; i--) {
+            // Extraction des informations de notre liaison actuelle
+            int currentConnDepStopID = timetable.connectionsFor(date).depStopId(i);
+            int currentConnArrStopID = timetable.connectionsFor(date).arrStopId(i);
+            int currentConnDepMins = timetable.connectionsFor(date).depMins(i);
+            int currentConnArrMins = timetable.connectionsFor(date).arrMins(i);
+            int currentConnTripId = timetable.connectionsFor(date).tripId(i);
+            int currentConnTripPos = timetable.connectionsFor(date).tripPos(i);
+
+            ParetoFront.Builder f = new ParetoFront.Builder();
+
+            // ------------------ Option 1) ---------------
+            // Si il existe un changement jusqu'à la gare
+            // d'arrivée (depuis la fin de notre liaison), on y marche
+
+            boolean changeToFinalDestinationExist;
+            int changeDuration = 0;
+
+            try {
+                changeDuration = timetable().transfers().minutesBetween(currentConnArrStopID, arrStationId);
+                changeToFinalDestinationExist = true;
+            } catch (NoSuchElementException e) {
+                changeToFinalDestinationExist = false;
+            }
+
+            if (changeToFinalDestinationExist) {
+                f.add(PackedCriteria.pack(currentConnArrMins + changeDuration, 0, 0)); // Payload 0
+            }
+
+            // ------------------ Option 2) ---------------
+            // On continue notre trajet normalement, et on
+            // ajoute à la frontière tous les tuples de ce trip (course)
+            f.addAll(profileBuilder.forTrip(currentConnTripId));
+
+
+            // ------------------ Option 3) ---------------
+            // Gère le changement de véhicule, donc les transitions entre les routes
+            profileBuilder.forStation(currentConnArrStopID).forEach();
 
 
         }
