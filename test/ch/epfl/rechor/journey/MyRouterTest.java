474125aa40190e9477f937cad8fc18609136ca63
Blaazen <blqzen@gmail.com>
2025-04-04 13:18:58 +0200

    debug

--- test/ch/epfl/rechor/journey/MyRouterTest.java
+++ test/ch/epfl/rechor/journey/MyRouterTest.java
@@ -23,24 +23,25 @@ class MyRouterTest {
     }
 
     @Test
-    public void routerTest() throws IOException {
+    void testJourneyRouting() throws IOException {
         long tStart = System.nanoTime();
 
-        TimeTable timeTable = new CachedTimeTable(FileTimeTable.in(Path.of("timetable")));
+        TimeTable timeTable =
+                new CachedTimeTable(FileTimeTable.in(Path.of("timetable")));
         Stations stations = timeTable.stations();
         LocalDate date = LocalDate.of(2025, Month.APRIL, 1);
         int depStationId = stationId(stations, "Ecublens VD, EPFL");
         int arrStationId = stationId(stations, "Gruyères");
         Router router = new Router(timeTable);
         Profile profile = router.profile(date, arrStationId);
+        Journey journey = JourneyExtractor
+                .journeys(profile, depStationId)
+                .get(32);
+        String icalEvent = JourneyIcalConverter.toIcalendar(journey);
 
-        var journeys = JourneyExtractor.journeys(profile, depStationId);
-        if (journeys.size() > 32) {
-            Journey journey = journeys.get(32);
-            System.out.println(JourneyIcalConverter.toIcalendar(journey));
-        } else {
-            System.out.println("Pas assez de voyages trouvés pour obtenir l'élément à l'index 32");
-        }
+        assertNotNull(icalEvent);
+        assertTrue(icalEvent.contains("BEGIN:VCALENDAR"));
+        assertTrue(icalEvent.contains("END:VCALENDAR"));
 
         double elapsed = (System.nanoTime() - tStart) * 1e-9;
         System.out.printf("Temps écoulé : %.3f s%n", elapsed);
