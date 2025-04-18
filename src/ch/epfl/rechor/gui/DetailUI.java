4213b6386768624713795810b66f822385904021
axel <axel.verga@epfl.ch>
2025-04-18 11:07:19 +0200

    Auto stash before merge of "main" and "origin/main"

--- src/ch/epfl/rechor/gui/DetailUI.java
+++ src/ch/epfl/rechor/gui/DetailUI.java
@@ -22,28 +22,23 @@ public record DetailUI(Node rootNode) {
      */
     public static DetailUI create(ObservableValue<Journey> journeyObservableValue) {
 
-<<<<<<< HEAD
+        // Noeud principal
+
         GridPane gridPane = new GridPane();
-=======
-        return GridPane();
 
-
-
->>>>>>> 867ece0595b56bd7a61f0b6263a591695781236b
 
         // colonne 0: heures de départ et d'arrivée, et icône du véhicule
-       // GridPane.setRowIndex(,0 );
+        // GridPane.setRowIndex(,0 );
 
 
         // colonne 1: cercles de départ et d'arrivée
-       // GridPane.setRowIndex(,1);
+        // GridPane.setRowIndex(,1);
 
         // colonne 2: nom de la gare de départ et d'arrivée
-       // GridPane.setRowIndex(,2);
+        // GridPane.setRowIndex(,2);
 
         // colonne 3: voie/quai de départ et d'arrivée
         //GridPane.setRowIndex(,3);
-
 
 
         return new DetailUI(gridPane);
