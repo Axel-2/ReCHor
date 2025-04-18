cbc13334cdb1536ad92909349cdf0aed34ea58ae
Blaazen <blqzen@gmail.com>
2025-04-18 10:36:27 +0200

    t

--- src/ch/epfl/rechor/gui/DetailUI.java
+++ src/ch/epfl/rechor/gui/DetailUI.java
@@ -4,6 +4,7 @@ import ch.epfl.rechor.journey.Journey;
 import javafx.beans.Observable;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.Node;
+import javafx.scene.layout.GridPane;
 
 /**
  * Classe qui représente la partie de l'interface
@@ -21,7 +22,22 @@ public record DetailUI(Node rootNode) {
      */
     public static DetailUI create(ObservableValue<Journey> journeyObservableValue) {
 
-        
+        GridPane gridPane = new GridPane();
 
+        // colonne 0: heures de départ et d'arrivée, et icône du véhicule
+       // GridPane.setRowIndex(,0 );
+
+        // colonne 1: cercles de départ et d'arrivée
+       // GridPane.setRowIndex(,1);
+
+        // colonne 2: nom de la gare de départ et d'arrivée
+       // GridPane.setRowIndex(,2);
+
+        // colonne 3: voie/quai de départ et d'arrivée
+        //GridPane.setRowIndex(,3);
+
+
+
+        return new DetailUI(gridPane);
     }
 }
